845d38b5bee7633ad06ebc5cb02523b4d6cb60e7
Philippa Johnstone <pecj@hotmail.co.uk>
2015-12-17 01:42:51 +0800

    mend

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -5,7 +5,7 @@ class Animal < ActiveRecord::Base
 
   has_attached_file :avatar,
                     styles: { medium: "300x300>", thumb: "100x100>" },
-                    default_url: "/app/assets/images/pet.jpg"
+                    default_url: "/pet.jpg"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
